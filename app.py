d498c0fc17dd15606e97f5471ede07701409e43c
Reda Grace <41922411+RedaGrace@users.noreply.github.com>
2022-07-04 19:36:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def request_page():
 
 if __name__=='__main__':
     port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port, debug=True)
-    serve(app, host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True)
+    #serve(app, host='0.0.0.0', port=port, debug=True)
+
